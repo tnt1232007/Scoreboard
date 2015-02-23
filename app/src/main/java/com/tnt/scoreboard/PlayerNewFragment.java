6e504296406eb30268919be89324a450a3f1dc84
TNT <tnt1232007@gmail.com>
2015-02-23 21:45:54 +0700

    [IMPLEMENT] Disable auto show dropdown

--- app/src/main/java/com/tnt/scoreboard/PlayerNewFragment.java
+++ app/src/main/java/com/tnt/scoreboard/PlayerNewFragment.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 TNT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.tnt.scoreboard;
 
 import android.app.Fragment;
@@ -90,14 +106,6 @@ public class PlayerNewFragment extends Fragment {
         mTextView.setHint(mPlayerName);
         mTextView.setAdapter(new ArrayAdapter<>(getActivity(),
                 android.R.layout.simple_dropdown_item_1line, getNameList()));
-        mTextView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    mTextView.showDropDown();
-                }
-            }
-        });
         return v;
     }
 
