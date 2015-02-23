7aef7765a7bc1e3710cd93582123e2f9059e8939
TNT <tnt1232007@gmail.com>
2015-02-24 01:27:18 +0700

    [FIX] Toolbar height when landscape

--- app/src/main/java/com/tnt/scoreboard/GameScoreActivity.java
+++ app/src/main/java/com/tnt/scoreboard/GameScoreActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 TNT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.tnt.scoreboard;
 
 import android.content.Intent;
@@ -18,6 +34,8 @@ import com.tnt.scoreboard.utils.DateTimeUtils;
 import java.util.List;
 
 //TODO: Right drawer show current game info
+//TODO: Edit players
+//TODO: Select all
 public class GameScoreActivity extends BaseActivity {
 
     public static final String ROUND = "Round ";
@@ -34,6 +52,36 @@ public class GameScoreActivity extends BaseActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
+        //TODO: Collapse toolbar on scroll
+//        recyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+//            @Override
+//            public void onScrolled(final RecyclerView recyclerView, int dx, int dy) {
+//                final int height = mToolbar.getHeight();
+//                if (dy > 0 && mToolbar.getTranslationY() == 0) {
+//                    mToolbar.animate()
+//                            .translationY(-height)
+//                            .withStartAction(new Runnable() {
+//                                @Override
+//                                public void run() {
+//                                    recyclerView.setPadding(0, 0, 0, 0);
+//                                }
+//                            })
+//                            .setInterpolator(new AccelerateInterpolator())
+//                            .start();
+//                } else if (dy < 0 && mToolbar.getTranslationY() == -height) {
+//                    mToolbar.animate()
+//                            .translationY(0)
+//                            .setInterpolator(new AccelerateInterpolator())
+//                            .withEndAction(new Runnable() {
+//                                @Override
+//                                public void run() {
+//                                    recyclerView.setPadding(0, height, 0, 0);
+//                                }
+//                            })
+//                            .start();
+//                }
+//            }
+//        });
 
         mPlayerAdapter = new PlayerAdapter(this, mGame);
         mPlayerAdapter.setListener(new PlayerAdapter.IOnScoreUpdateListener() {
